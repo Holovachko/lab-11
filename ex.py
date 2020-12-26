64d4a5819e88e6b031b2c4b82228d647996a6d0d
Holovachko <74302993+Holovachko@users.noreply.github.com>
2020-12-26 22:39:26 +0200

    Create ex.py

---
+++ ex.py
@@ -0,0 +1,39 @@
+import math
+
+
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.vector = [el for el in range(int(math.sqrt(self.x**2 + self.y**2)+1))]
+
+    @property
+    def x(self):
+        return self.__x
+    @x.setter
+    def x(self,a):
+        self.__x = a
+    @property
+    def y(self):
+        return self.__y
+    @y.setter
+    def y(self,a):
+        self.__y = a
+        
+
+
+    def vectors_len(self):
+        return self.vector
+    def rationing_vector(self):
+        return self.x/self.vectors_len(),self.y/self.vectors_len()
+
+    def __gt__(self, other):
+        return self.vectors_len() > other.vectors_len()
+    def __lt__(self, other):
+        return self.vectors_len() < other.vectors_len()
+    def __add__(self, other):
+        return (other.x + self.x , other.y + self.y)
+    def __sub__(self, other):
+        return (self.x - other.x, self.y - other.y)
+    def __mul__(self, other):
+        return (self.x *other.x + self.y * other.y)
